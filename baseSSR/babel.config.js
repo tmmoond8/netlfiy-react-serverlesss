f895651ba8d0ec7c64b2fa9197d3f89ef69e71cb
tamm <tmmoont8@gmail.com>
2021-08-16 18:00:16 +0900

    ✨ create project - rm tuto project

--- baseSSR/babel.config.js
+++
@@ -1,3 +0,0 @@
-const presets = ['@babel/preset-react', '@babel/preset-env'];
-const plugins = ['@babel/plugin-proposal-class-properties'];
-module.exports = { presets, plugins };
--- craSSR/src/pages/About.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function About() {
-  return (
-    <div>
-      <h3>This is about page</h3>
-    </div>
-  )
-}
