28bd3afbd27fe12c3ecb9bb5987de6495ecbebe4
tmmoond8 <tmmoond8@gmail.com>
2021-08-24 14:07:53 +0900

    💚 netlify build fail

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,19 @@
 const { 
   override,
 } = require("customize-cra");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   webpack: override(
       (config) => {
-        console.log(config);
         config.output.filename = 'static/js/[name].js'
         config.output.chunkFilename = 'static/js/[name].chunk.js'
+        config.plugins[0].options.filename = 'server.html'
+        // console.log('plugins', config.plugins[0].options);
+
+        // console.log('aaa', Object.keys(config.plugins[0]));
+        // const htmlWebpackPlugin = config.plugins.find((plugin) => plugin instanceof HtmlWebpackPlugin);
+        // console.log('dd', htmlWebpackPlugin);
         return config;
       }
   )
