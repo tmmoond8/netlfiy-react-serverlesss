dee113c80326382b32cc8baae6b71c011b76e0cd
tmmoond8 <tmmoond8@gmail.com>
2021-08-26 21:57:47 +0900

    📝 test

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Cat } from '../icons';
-import image from './image.png';
 
 export default function About() {
   return (
     <Page>
       <h3>This is about page</h3>
       <Cat />
-      <img src={image} />
     </Page>
   )
 }
