9892bfca9bccd8e38b495b26b059ae338bbdb5d7
tmmoond8 <tmmoond8@gmail.com>
2021-08-30 10:52:10 +0900

    ✨ netlify-function develop env

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function Home() {
+  const [ data, setData ] = useState(null);
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get('/.netlify/functions/getTopic');
+      setData(data);
+    })();
+  }, [])
   return (
     <div>
       HOME
+      <p>
+        {JSON.stringify(data)}
+      </p>
     </div>
   );
 }
